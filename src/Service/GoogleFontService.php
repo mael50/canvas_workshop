289a0d6d1957f4918c24739e4c774f0d9471c96b
Mael <maellaroque50@gmail.com>
2024-09-26 09:16:31 +0200

    add cache for fonts

--- src/Service/GoogleFontService.php
+++ src/Service/GoogleFontService.php
@@ -2,31 +2,40 @@
 
 namespace App\Service;
 
+use Symfony\Contracts\Cache\ItemInterface;
+use Symfony\Contracts\Cache\CacheInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class GoogleFontService
 {
     private $client;
     private $apiKey;
+    private $cache;
 
-    public function __construct(HttpClientInterface $client, string $apiKey)
+    public function __construct(HttpClientInterface $client, string $apiKey, CacheInterface $cache)
     {
         $this->client = $client;
         $this->apiKey = $apiKey;
+        $this->cache = $cache;
     }
 
     public function getFonts()
     {
-        $response = $this->client->request(
-            'GET',
-            'https://www.googleapis.com/webfonts/v1/webfonts',
-            [
-                'query' => [
-                    'key' => $this->apiKey,
-                ],
-            ]
-        );
 
-        return $response->toArray();
+        return $this->cache->get('google_fonts', function (ItemInterface $item) {
+            $item->expiresAfter(36000);
+
+            $response = $this->client->request(
+                'GET',
+                'https://www.googleapis.com/webfonts/v1/webfonts',
+                [
+                    'query' => [
+                        'key' => $this->apiKey,
+                    ],
+                ]
+            );
+
+            return $response->toArray();
+        });
     }
 }
